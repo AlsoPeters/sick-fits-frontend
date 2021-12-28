8583fb84d9b7c9034f2aa909816b818091c04872
AlsoPeters <kyle.fire@gmail.com>
2021-12-28 11:29:35 +0900

    [23] Hooking up File input and Form Styles

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -2,6 +2,7 @@ import useForm from "../lib/useForm";
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: "",
     name: "Nice shoes",
     price: 1234,
     description: "These are the best shoes!",
@@ -9,43 +10,65 @@ export default function CreateProduct() {
 
   return (
     <div>
-      <form>
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            value={inputs.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="price">
-          price
-          <input
-            type="number"
-            id="name"
-            name="price"
-            placeholder="Price"
-            value={inputs.price}
-            onChange={handleChange}
-          />
-        </label>
-        <button
-          className="py-1 px-2 m-2 rounded-md border-2 bg-tokyo-term-black border-tokyo-term-magenta"
-          type="button"
-          onClick={clearForm}
-        >
-          Clear Form
-        </button>
-        <button
-          className="py-1 px-2 m-2 rounded-md border-2 bg-tokyo-term-black border-tokyo-term-magenta"
-          type="button"
-          onClick={resetForm}
-        >
-          Reset Form
-        </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(inputs);
+        }}
+      >
+        <fieldset>
+          <label htmlFor="image">
+            Image
+            <input
+              required
+              type="file"
+              id="image"
+              name="image"
+              onChange={handleChange}
+            />
+          </label>
+
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            price
+            <input
+              type="number"
+              id="name"
+              name="price"
+              placeholder="Price"
+              value={inputs.price}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Description"
+              value={inputs.description}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button
+            // TODO: add this button style as a default
+            className="py-1 px-2 m-2 rounded-md border-2 bg-tokyo-term-black border-tokyo-term-magenta"
+            type="submit"
+          >
+            + Add Product
+          </button>
+        </fieldset>
       </form>
     </div>
   );
