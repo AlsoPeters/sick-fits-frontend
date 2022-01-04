f6e552d933072e0aceceeba8e9926c7710a71919
AlsoPeters <Kyle.fire@gmail.com>
2022-01-04 13:26:19 +0900

    [26] - Programmatically Changing the Page after product creation

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import DisplayError from '../components/ErrorMessage';
 import { ALL_PRODUCTS_QUERY } from './Products';
+import Router from 'next/router';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -50,10 +51,13 @@ export default function CreateProduct() {
       <form
         onSubmit={async (e) => {
           e.preventDefault();
-          console.log(inputs);
           // Submit the inputfields to the backend:
-          await createProduct();
+          const res = await createProduct();
           clearForm();
+          // Go to that products page
+          Router.push({
+            pathname: `/product/${res.data.createProduct.id}`,
+          });
         }}
       >
         <DisplayError error={error} />
