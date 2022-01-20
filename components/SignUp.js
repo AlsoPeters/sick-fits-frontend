4a51e2766b5ab20aa391c3497db2ac620938a470
AlsoPeters <Kyle.fire@gmail.com>
2022-01-20 16:37:35 +0900

    [39] - Creating our Sign Up Flow

--- components/SignUp.js
+++ components/SignUp.js
@@ -4,56 +4,67 @@ import { useMutation } from '@apollo/client';
 import { CURRENT_USER_QUERY } from './User';
 import Error from './ErrorMessage';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignUp() {
+export default function signup() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
+    name: '',
     password: '',
   });
 
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
     // refetch the currently logged in user
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
   async function handleSubmit(e) {
     e.preventDefault(); // stop the form from submitting
     console.log(inputs);
-    await signin();
+    const res = await signup().catch(console.error);
+    console.log(res);
+    console.log({ data, loading, error });
 
     resetForm();
     // Send the email and password to the graphqlAPI
   }
 
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    'UserAuthenticationWithPasswordFailure'
-      ? data?.authenticateUserWithPassword
-      : undefined;
   return (
     <form className='flex' method='POST' onSubmit={handleSubmit}>
       <fieldset className='px-2 border-2 rounded-sm border-tokyo-term-magenta'>
+        {data?.createUser && (
+          <p className='my-2 text-xl font-bold text-tokyo-term-green'>
+            Signed up with {data.createUser.email} - Please go head and sign in.
+          </p>
+        )}
         <h2 className='text-xl font-bold text-tokyo-term-white '>
-          Sign In to Your Account
+          Sign Up For an Account
         </h2>
         <Error error={error} />
+        <label className='flex flex-col my-2 font-bold' htmlFor='email'>
+          Your Name
+          <input
+            className='px-2 border rounded-sm text-tokyo-term-white bg-tokyo-comment_PURP border-tokyo-term-magenta'
+            type='text'
+            name='name'
+            placeholder='Dohn Joe'
+            autoComplete='name'
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
         <label className='flex flex-col my-2 font-bold' htmlFor='email'>
           Email
           <input
@@ -66,6 +77,7 @@ export default function SignUp() {
             onChange={handleChange}
           />
         </label>
+
         <label className='flex flex-col my-2 font-bold' htmlFor='password'>
           Password
           <input
