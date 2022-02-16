2c4e9cf5e01cb8fe5f53de7c44992cc363284b5a
AlsoPeters <Kyle.fire@gmail.com>
2022-02-16 12:28:06 +0900

    [60] - Displaying All Orders

--- components/Page.js
+++ components/Page.js
@@ -1,10 +1,9 @@
-import Header from "./Header";
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
-    <div className="max-h-max bg-tokyo-night_BLK text-tokyo-term-blue">
+    <div className='max-h-max bg-tokyo-night_BLK text-tokyo-term-blue'>
       <Header />
-      <h2 className="">I am the page component</h2>
       {children}
     </div>
   );
