8ab018641bd47975eb6b771888efd06dd8ba0d07
AlsoPeters <kyle.fire@gmail.com>
2022-03-08 11:08:24 +0900

    [78] - Updating Props and re-rendering

---
+++ __tests__/CartCount.test.js
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+import wait from 'waait';
+import CartCount from '../components/CartCount';
+
+describe('<CartCount/>', () => {
+  it('Renders', () => {
+    render(<CartCount count={10} />);
+  });
+  it('Matches snapshot', () => {
+    const { container } = render(<CartCount count={11} />);
+    expect(container).toMatchSnapshot();
+  });
+  it('Updates via props', () => {
+    const { container, rerender, debug } = render(<CartCount count={11} />);
+    expect(container.textContent).toBe('11');
+    // expect(container).toHaveTextContent('11');
+    // Update the props
+    rerender(<CartCount count={12} />);
+    debug();
+    expect(container.textContent).toBe('12');
+    expect(container);
+  });
+});
