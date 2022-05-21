3facd4ed719c740a39e5812661f7b75ed82f24d6
AlsoPeters <kyle.fire@gmail.com>
2022-05-21 19:14:15 +0900

    adding meta data

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
-        <body className=" h-max bg-tokyo-night_BLK">
+        <Head>
+          <meta charset='utf-8' />
+          <meta http-equiv='X-UA-Compatible' content='IE=11' />
+          <meta http-equiv='Cache-Control' content='no-cache' />
+          <meta
+            http-equiv='Content-Security-Policy'
+            content="default-src 'self'; style-src 'self';"
+          />
+        </Head>
+        <body className=' h-max bg-tokyo-night_BLK'>
           <Main />
           <NextScript />
         </body>
