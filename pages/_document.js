2e43058e5ccb946bbe6d4b3dfc8cbcc6e386ce5e
AlsoPeters <kyle.fire@gmail.com>
2022-05-21 20:14:27 +0900

    removing meta data

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charset='utf-8' />
-          <meta http-equiv='X-UA-Compatible' content='IE=11' />
-          <meta http-equiv='Cache-Control' content='no-cache' />
-          <meta
-            http-equiv='Content-Security-Policy'
-            content="default-src 'self'; style-src 'self';"
-          />
-        </Head>
+        <Head></Head>
         <body className=' h-max bg-tokyo-night_BLK'>
           <Main />
           <NextScript />
