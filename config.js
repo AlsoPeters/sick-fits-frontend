bc94651f2f036203f2ad18c3ecbc08eb5b53cefd
AlsoPeters <kyle.fire@gmail.com>
2022-05-22 12:56:36 +0900

    for rebuild

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
 // export const endpoint = `https://api.alsopeters.dev/api/graphql`;
+// hey
 export const prodEndpoint = `https://api.alsopeters.dev/api/graphql`;
 export const perPage = 6;
